996dd9ca4d31312b95e3ba30cfd69ecbdfe9985b
Karine Dias <harlequinuniform@gmail.com>
2021-10-05 11:05:21 +0200

    Add gitignore file

---
+++ back/src/main/java/com/karinedias/hotelapp/exceptions/InvalidEntityException.java
@@ -0,0 +1,8 @@
+package com.karinedias.hotelapp.exceptions;
+
+public class InvalidClientException extends Throwable {
+    
+    public InvalidClientException(String message) {
+        super(message);
+    }
+}
