15cf0b31b785f08a9314b0f9d816a36ca596c8ad
Karine Dias <harlequinuniform@gmail.com>
2021-10-06 18:03:02 +0200

    Fix navbar element in header

--- front/src/app/auth.guard.ts
+++ front/src/app/auth.guard.ts
@@ -13,14 +13,8 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-      //TODO: get user name to welcome in main page
-      const user = sessionStorage.getItem('connectedUser');
-      console.log(user);
-      
-    // sessionStorage stocke les données pour une connexion
     if (sessionStorage.getItem("connectedUser"))
       return true;
-
       // if user not connected, redirect them to the login route
     this.router.navigate(['login']);
     return false;
