fe2244b16ee7b4c53787e748e44119742e4e9b5d
Karine Dias <harlequinuniform@gmail.com>
2021-10-27 09:53:36 +0200

    Fix DML statements to not insert rows if already present

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -7,23 +7,23 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO `admin` (`id`, `username`, `password`, `role`) VALUES
+INSERT IGNORE INTO `admin` (`id`, `username`, `password`, `role`) VALUES
 (1, 'test', '$2a$12$WVbut.2BADbtUFxrJ0E6EOznb/IeH5R0uh0ivRIwx3Lx2Qr1fqWh.', 'ROLE_ADMIN'),
 (2, 'admin', '$2a$12$nF2pw0ZPBzrsn9zVunxIU.aFDwXR4kvDKcf2sExqkwe7lh9GFvSPW', 'ROLE_ADMIN');
 
-INSERT INTO `client` (`id`, `nom_complet`, `telephone`, `email`, `adresse`) VALUES
+INSERT IGNORE INTO `client`  (`id`, `nom_complet`, `telephone`, `email`, `adresse`) VALUES
 (2, 'Manon Fanfrelune', '0235668917', 'm.fanfrelune@company.gov', '56 rue de la Loire, 44000 Nantes'),
 (3, 'Thierry Lalongue', '0341274302', 'thierrylalongueg@yahoo.com', '1bis rue des Pommiers, 78000 Versailles'),
 (5, 'Crique Lucien', '0688297210', 'luciencrique@random.com', '16 boulevard de la République'),
 (6, 'Trouville Philippe', '0453672800', 'phil-trouville@gmail.com', '17bis rue du Temple');
 
-INSERT INTO `hotel` (`id`, `nom`, `etoiles`, `adresse`, `telephone`, `email`, `ville`) VALUES
+INSERT IGNORE INTO `hotel` (`id`, `nom`, `etoiles`, `adresse`, `telephone`, `email`, `ville`) VALUES
 (1, 'Saint-Christophe', 3, '12 avenue du Maréchal Leclerc', '0144223817', 'hotel-stchristophe@resa.com', 'Marseille'),
 (2, 'Malte', 4, '118 boulevard Saint-Martin', '0145323976', 'hotelmalte@resa.fr', 'Paris'),
 (3, 'Le Mariott', 5, '15 avenue Polichinelle', '0128736640', 'mariott@hotel.com', 'St Tropez'),
 (4, 'El Sombrero', 2, '64 rue de la Fourmi', '0128836640', 'elsombrero@hotmail.com', 'Nice');
 
-INSERT INTO `reservation` (`id`, `client`, `hotel`, `date_debut`, `date_fin`, `num_chambre`) VALUES
+INSERT IGNORE INTO `reservation` (`id`, `client`, `hotel`, `date_debut`, `date_fin`, `num_chambre`) VALUES
 (1, 2, 3, '2021-10-08', '2021-10-10', 233),
 (2, 3, 2, '2021-10-14', '2021-10-16', 26),
 (3, 2, 4, '2021-11-08', '2021-11-10', 404),
